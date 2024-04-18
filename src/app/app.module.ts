c3c8cb610223b6db36915452e18453ef21920616
manishaka <manishaka@cybage.com>
2024-04-18 14:13:31 +0530

    booking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { PaymentConfirmationComponent } from './BookingApp/payment-confirmation/
 import { WalletComponent } from './BookingApp/wallet/wallet.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminModule } from './admin/admin.module';
+import { EventModule } from './event/event.module';
+import { MovieModule } from './movie/movie.module';
 
 @NgModule({
   declarations: [
@@ -31,44 +34,16 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    FontAwesomeModule 
-    // RouterModule.forRoot([
-    //   { path: 'add-booking', component: BookingComponent },
-    //   { path: 'add-confirm-booking', component: ConfirmBookingComponent }
-    // ])
-  ],
-  providers: [BookingService, provideAnimationsAsync()],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MovieModule } from './movie/movie.module';
-import { FormsModule } from '@angular/forms';
-import { AdminModule } from './admin/admin.module';
-import { AddMovieComponent } from './movie/add-movie/add-movie.component';
-import { EventModule } from './event/event.module';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
+    FontAwesomeModule ,
     MovieModule,
     FormsModule,
     AdminModule,
     EventModule
     
-
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [BookingService, provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
